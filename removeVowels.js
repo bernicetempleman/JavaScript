8a6c3eac098df03f6a883b8728783d7419a33b8b
Bernice Templeman <bernice.templeman@gmail.com>
2022-01-14 11:19:43 -0500

    Add files via upload

---
+++ removeVowels.js
@@ -0,0 +1,14 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+ const s = readline();
+
+ // Write an answer using console.log()
+ // To debug: console.error('Debug messages...');
+  string = s.replace( /[aeiouAEIOU]/g, '' );              
+ 
+ 
+ console.log(string);
+ 
